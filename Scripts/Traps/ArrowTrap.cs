10dc6b828f2b3869c6dc286a0144dcc1a93f827e
Arseniy2025 <arseniysergeev06@gmail.com>
2025-12-23 17:30:28 +0700

    Update ArrowTrap.cs

--- Scripts/Traps/ArrowTrap.cs
+++ Scripts/Traps/ArrowTrap.cs
@@ -1,35 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// Дополнительные функции, которые можно добавить:
 
-public class ArrowTrap : MonoBehaviour
+// 1. Направление выстрела
+[SerializeField] private Vector2 shootDirection = Vector2.right;
+
+// 2. Случайный разброс
+[SerializeField] private float spreadAngle = 5f;
+
+// 3. Анимация выстрела
+private Animator animator;
+
+
+private void Start()
 {
-    [SerializeField] private float attackCooldown;
-    [SerializeField] private Transform firePoint;
-    [SerializeField] private GameObject[] arrows;
-    private float cooldownTimer;
+    // Инициализация компонентов
+    animator = GetComponent<Animator>();
+}
 
-    private void Attack()
-    {
-        cooldownTimer = 0;
+private void Attack()
+{
+    cooldownTimer = 0;
+    
+    // Воспроизведение анимации
+    if (animator != null)
+        animator.SetTrigger("Shoot");
+    
+    
+    // Получение стрелы
+    int arrowIndex = FindArrow();
+    if (arrowIndex == -1) return; // Если стрел нет
+    
+    // Установка позиции и направления
+    GameObject arrow = arrows[arrowIndex];
+    arrow.transform.position = firePoint.position;
+    
+    // Добавление случайного разброса
+    float randomAngle = Random.Range(-spreadAngle, spreadAngle);
+    Quaternion rotation = Quaternion.Euler(0, 0, randomAngle);
+    arrow.transform.rotation = rotation;
+    
+    // Активация
+    arrow.GetComponent<EnemyProjectle>().ActivateProjectile();
+}
 
-        arrows[FindArrow()].transform.position = firePoint.position;
-        arrows[FindArrow()].GetComponent<EnemyProjectle>().ActivateProjectile();
-    }
-    private int FindArrow()
+// Улучшенный метод поиска
+private int FindArrow()
+{
+    for (int i = 0; i < arrows.Length; i++)
     {
-        for (int i = 0; i < arrows.Length; i++)
-        {
-            if (!arrows[i].activeInHierarchy)
-                return i;
-        }
-        return 0;
+        if (!arrows[i].activeInHierarchy)
+            return i;
     }
-    private void Update()
-    {
-        cooldownTimer += Time.deltaTime;
-
-        if (cooldownTimer >= attackCooldown)
-            Attack();
-    }
+    return -1; // Явное указание, что стрел нет
 }
