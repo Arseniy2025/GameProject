caf3915f429845736f350a2806880c4c8f9e2783
Arseniy2025 <arseniysergeev06@gmail.com>
2025-12-23 17:29:11 +0700

    Update UIManager.cs

--- Scripts/UI/UIManager.cs
+++ Scripts/UI/UIManager.cs
@@ -3,66 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Менеджер пользовательского интерфейса, управляющий экранами паузы и завершения игры
 public class UIManager : MonoBehaviour
 {
     [Header("Game Over")]
-    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private GameObject gameOverScreen; // Экран завершения игры
 
     [Header("Pause")]
-    [SerializeField] private GameObject pauseScreen;
+    [SerializeField] private GameObject pauseScreen; // Экран паузы
 
     private void Awake()
     {
+        // Инициализация: скрываем оба экрана при запуске
         gameOverScreen.SetActive(false);
         pauseScreen.SetActive(false);
     }
+    
     private void Update()
     {
+        // Проверка нажатия клавиши Escape для паузы
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            //If pause screen already active unpause and viceversa
+            // Если экран паузы уже активен - снимаем паузу, иначе ставим на паузу
             PauseGame(!pauseScreen.activeInHierarchy);
         }
     }
 
     #region Game Over
-    //Activate game over screen
+    // Активация экрана завершения игры
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
     }
 
-    //Restart level
+    // Перезапуск текущего уровня
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    //Main Menu
+    // Возврат в главное меню
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
     }
 
-    //Quit game/exit play mode if in Editor
+    // Выход из игры / выход из режима игры в редакторе
     public void Quit()
     {
-        Application.Quit(); //Quits the game (only works in build)
+        Application.Quit(); // Закрытие игры (работает только в сборке)
 
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; //Exits play mode (will only be executed in the editor)
+        UnityEditor.EditorApplication.isPlaying = false; // Выход из режима игры (выполняется только в редакторе)
 #endif
     }
     #endregion
 
     #region Pause
+    // Управление паузой в игре
     public void PauseGame(bool status)
     {
-        //If status == true pause | if status == false unpause
+        // Если status == true - ставим на паузу | если status == false - снимаем с паузы
         pauseScreen.SetActive(status);
 
-        //When pause status is true change timescale to 0 (time stops)
-        //when it's false change it back to 1 (time goes by normally)
+        // Когда игра на паузе, устанавливаем timescale в 0 (время останавливается)
+        // когда пауза снята, возвращаем timescale в 1 (время идет нормально)
         if (status)
             Time.timeScale = 0;
         else
@@ -70,4 +75,3 @@ public class UIManager : MonoBehaviour
     }
     #endregion
 }
-
